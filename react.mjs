b64f58b450600f04c476e18928ab1404e72f8b06
tiye <jiyinyiyong@gmail.com>
2025-10-03 15:06:41 +0800

    try layout components

--- react.mjs
+++ react.mjs
@@ -2,11 +2,22 @@ import * as React from "react";
 import * as ReactDOMClient from "react-dom/client";
 import "@ant-design/v5-patch-for-react-19";
 
-import { Button, FloatButton, Typography } from "antd";
+import { 
+  Button, FloatButton, Typography, Divider, Flex, Space, Layout, Breadcrumb, Menu, Steps,
+  Input, Checkbox, Radio, Switch,
+  Avatar, Badge, Card, Tag, Tooltip,
+  Alert, Modal, Progress, Spin, Result
+} from "antd";
 import * as Icon from "@ant-design/icons";
 
 console.log(ReactDOMClient);
 
 window.React = React;
 window.ReactDOMClient = ReactDOMClient;
-window.AntD = { Button, FloatButton, Typography, Icon };
+window.AntD = { 
+  Button, FloatButton, Typography, Divider, Flex, Space, Layout, Breadcrumb, Menu, Steps,
+  Input, Checkbox, Radio, Switch,
+  Avatar, Badge, Card, Tag, Tooltip,
+  Alert, Modal, Progress, Spin, Result,
+  Icon 
+};
