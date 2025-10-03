50016db616ef8fd936cc7a5fd6a163e99659a5e0
tiye <jiyinyiyong@gmail.com>
2025-10-03 14:34:20 +0800

    connect Typography component

--- react.mjs
+++ react.mjs
@@ -2,11 +2,11 @@ import * as React from "react";
 import * as ReactDOMClient from "react-dom/client";
 import "@ant-design/v5-patch-for-react-19";
 
-import { Button, FloatButton } from "antd";
+import { Button, FloatButton, Typography } from "antd";
 import * as Icon from "@ant-design/icons";
 
 console.log(ReactDOMClient);
 
 window.React = React;
 window.ReactDOMClient = ReactDOMClient;
-window.AntD = { Button, FloatButton, Icon };
+window.AntD = { Button, FloatButton, Typography, Icon };
