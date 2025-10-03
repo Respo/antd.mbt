4925504b59aa08267009c5d5da66184e54c22328
tiye <jiyinyiyong@gmail.com>
2025-10-03 12:33:51 +0800

    types for flatbutton

--- react.mjs
+++ react.mjs
@@ -2,10 +2,10 @@ import * as React from "react";
 import * as ReactDOMClient from "react-dom/client";
 import "@ant-design/v5-patch-for-react-19";
 
-import { Button } from "antd";
+import { Button, FloatButton } from "antd";
 
 console.log(ReactDOMClient);
 
 window.React = React;
 window.ReactDOMClient = ReactDOMClient;
-window.AntD = { Button };
+window.AntD = { Button, FloatButton };
